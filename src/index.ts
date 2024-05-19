648740f051aaea041d49a630ab28c3852ccf5d67
rengated <kazievserg@gmail.com>
2024-05-19 20:59:48 +0300

    refatoring

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,70 @@
 import TelegramBot from "node-telegram-bot-api";
-import { commands, modelsId } from "./bot/config/config.js";
-import { handleQuestion } from "./bot/handlers/handleQuestion.js";
+import { commands } from "./bot/config/config.js";
+import { handleUser, handleQuestion } from "./bot/handlers/others/index.js";
+import { handleStart, handelMode, handelProfile } from "./bot/handlers/commands/index.js";
 import "dotenv/config";
 
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-
-const Bot = new TelegramBot(process.env.BOT_KEY!, {
+const bot = new TelegramBot(process.env.BOT_KEY!, {
   polling: true,
 });
-Bot.setMyCommands(commands);
 
-// Обработчик текстовых сообщений
-Bot.on("text", async (message) => {
-  //Находим запись с чатайди пользователя
-  const user = await prisma.users.findFirst({
-    where: {
-      chat_id: message.chat.id,
-    },
-  });
-  console.log("user", user);
-  if (!user) {
-    await prisma.users.create({ data: { chat_id: message.chat.id, model_id: 1 } });
-  }
-  if (message.text! == "/start") {
-    await Bot.sendMessage(message.chat.id, "Вы запустили бота!");
+bot.setMyCommands(commands);
+
+const commandsHandlers: { [key: string]: any } = {
+  "/start": handleStart,
+  "/profile": handelProfile,
+  "/mode": handelMode,
+};
+
+bot.on("text", async (message) => {
+  const user = await handleUser(message);
+  if (Object.keys(commandsHandlers).includes(message.text as string)) {
+    const messageHandler = commandsHandlers[message.text as string];
+    await messageHandler({ user, message, bot });
     return;
   }
-  if (message.text! == "/profile") {
-    await Bot.sendMessage(message.chat.id, `Ваша выбранная модель: ${user!.model_id}`);
-    return;
-  }
-  if (message.text! == "/mode") {
-    await Bot.sendMessage(message.chat.id, "Выберите модель", {
-      reply_markup: {
-        inline_keyboard: [...modelsId.map((model) => [{ text: model, callback_data: model }]), [{ text: "Закрыть Меню", callback_data: "closeMenu" }]],
-      },
-    });
-    return;
-  }
-  await handleQuestion(Bot, message, user!);
+
+  await handleQuestion({ user, message, bot });
+
+  // if (message.text! == "/start") {
+  //   await Bot.sendMessage(message.chat.id, "Вы запустили бота!");
+  //   return;
+  // }
+  // if (message.text! == "/profile") {
+  //   await Bot.sendMessage(message.chat.id, `Ваша выбранная модель: ${user!.model_id}`);
+  //   return;
+  // }
+  // if (message.text! == "/mode") {
+  //   await Bot.sendMessage(message.chat.id, "Выберите модель", {
+  //     reply_markup: {
+  //       inline_keyboard: [...modelsId.map((model) => [{ text: model, callback_data: model }]), [{ text: "Закрыть Меню", callback_data: "closeMenu" }]],
+  //     },
+  //   });
+  //   return;
+  // }
+  // await handleQuestion(Bot, message, user!);
 });
 
-Bot.on("callback_query", async (ctx) => {
-  try {
-    if (ctx.data == "closeMenu") {
-      await Bot.deleteMessage(ctx.message!.chat.id, ctx.message!.message_id);
-      return;
-    }
-    if (modelsId.includes(ctx.data!)) {
-      const model = await prisma.model.findFirst({
-        where: {
-          name: ctx.data,
-        },
-      });
-      await prisma.users.update({
-        where: { chat_id: ctx.message!.chat.id },
-        data: { model_id: model?.model_id },
-      });
-      await Bot.sendMessage(ctx.message!.chat.id, `Установлена ${ctx.data} версия`);
-      return;
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
+// Bot.on("callback_query", async (ctx) => {
+//   try {
+//     if (ctx.data == "closeMenu") {
+//       await Bot.deleteMessage(ctx.message!.chat.id, ctx.message!.message_id);
+//       return;
+//     }
+//     if (modelsId.includes(ctx.data!)) {
+//       const model = await prisma.model.findFirst({
+//         where: {
+//           name: ctx.data,
+//         },
+//       });
+//       await prisma.users.update({
+//         where: { chat_id: ctx.message!.chat.id },
+//         data: { model_id: model?.model_id },
+//       });
+//       await Bot.sendMessage(ctx.message!.chat.id, `Установлена ${ctx.data} версия`);
+//       return;
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
