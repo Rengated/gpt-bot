6a86a2a1cf224def909fb452d8f6000bdd0df2ba
rengated <kazievserg@gmail.com>
2024-05-20 14:42:44 +0300

    added image models

---
+++ src/api/answer/getImage.ts
@@ -0,0 +1,17 @@
+import { $api } from "../index.js";
+import { getImageTemplate } from "../templates/getImageTemplate.js";
+import { PrismaClient, Users } from "@prisma/client";
+
+export const getImage = async (question: string, model: string, user: Users, callback: (user: Users) => Promise<void>): Promise<string> => {
+  const prisma = new PrismaClient();
+  const template = getImageTemplate(question, model);
+  try {
+    const response = await $api.post("images/generations", template);
+    console.log(response.data);
+    callback(user);
+    return response.data.data[0].url;
+  } catch (err) {
+    console.log(err);
+    return "";
+  }
+};
