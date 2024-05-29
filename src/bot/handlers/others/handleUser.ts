65ca2eaa6e33c767210df67ab2320a498bdb20f1
rengated <kazievserg@gmail.com>
2024-05-30 01:51:06 +0300

    Обновил схему

--- src/bot/handlers/others/handleUser.ts
+++ src/bot/handlers/others/handleUser.ts
@@ -17,7 +17,7 @@ export const handleUser = async (message: Message) => {
         referral_link: generateRefLink(message.chat.id),
       },
     });
-    await prisma.user_subscriptions.create({
+    await prisma.userSubscriptions.create({
       data: {
         chat_id: String(message.chat.id),
         subscription_id: 1,
@@ -26,13 +26,19 @@ export const handleUser = async (message: Message) => {
       },
     });
 
-    const models = await prisma.models.findMany();
-    await prisma.requests.createMany({
+    const limits = await prisma.subscriptionLimits.findMany({
+      where: {
+        subscription_id: 1,
+      },
+    });
+
+    await prisma.userLimits.createMany({
       data: [
-        ...models.map((model) => ({
-          chat_id: String(message.chat.id),
-          count: 0,
-          model_id: model.id,
+        ...limits.map((limit) => ({
+          chat_id: String(user!.chat_id),
+          model_id: limit.model_id,
+          requests: 0,
+          limit: limit.count,
         })),
       ],
     });
